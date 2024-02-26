836ca677d5b5050bb18c46eeddd776c5428ac8d6
vasenkom <m.vasenko@yandex.ru>
2024-02-26 13:03:07 +0100

    add button:hover, ul li span and ul li span:hover

--- ToDo.js
+++ ToDo.js
@@ -1,12 +1,16 @@
 const inputBox = document.getElementById('input_box');
 const listContainer = document.getElementById('list_container');
 function addTask() {
-    if(inputBox.value === ''){
+    if (inputBox.value === ''){
         alert('Please add the task');
     }
-    else{
+    else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
+    inputBox.value = "";
 }
