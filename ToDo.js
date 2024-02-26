ddda2d3e534205c9a10a4b46cce60b0b42c4af08
vasenkom <m.vasenko@yandex.ru>
2024-02-26 14:23:35 +0100

    correct css: position of tasks, position and font size of date

--- ToDo.js
+++ ToDo.js
@@ -10,6 +10,7 @@ function addTask() {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+
         let span = document.createElement('span');
         span.innerHTML = '\u00d7';
         li.appendChild(span);
@@ -19,13 +20,13 @@ function addTask() {
 }
 
 //function to make the task checked and to delete the task
-listContainer.addEventListener('click', function(e){
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle('checked');
+listContainer.addEventListener('click', function(evt){
+    if(evt.target.tagName === "LI") {
+        evt.target.classList.toggle('checked');
         saveData();
     }
-    else if(e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
+    else if(evt.target.tagName === "SPAN") {
+        evt.target.parentElement.remove();
         saveData();
     }
 }, false)
@@ -39,4 +40,5 @@ function saveData() {
 function showTask() {
     listContainer.innerHTML = localStorage.getItem('data');
 }
+
 showTask();
